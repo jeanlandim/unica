1a19442673ad8fdfb39aea4c520e6a6b49f62166
Jean Landim <jewanbb@gmail.com>
2020-01-29 21:24:20 -0300

    finalizada pagina sobre a empresa

--- app/views.py
+++ app/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponseRedirect, request
 from .models import AppTextos
 # Página inicial do aplicativo
 def index(request):
+    #titulo = AppTextos.objects.get(pagina__startswith='/').titulo
+    #texto = AppTextos.objects.get(pagina__startswith='/').texto
     return render(request,'index.html')
 def servicos(request):
     return render(request,'servicos.html')
 def agenda(request):
     return render(request,'agenda.html')
 def fale_conosco(request):
-    return render(request,'fale_conosco.html')
+    titulo = AppTextos.objects.get(pagina__startswith='fale-conosco/').titulo
+    texto = AppTextos.objects.get(pagina__startswith='fale-conosco/').texto
+    return render(request,'fale_conosco.html',{'titulo':titulo,'texto':texto})
 def sobre(request):
     titulo = AppTextos.objects.get(pagina__startswith='sobre/').titulo
     texto = AppTextos.objects.get(pagina__startswith='sobre/').texto
