8834472290c7422445f2d84b4d6a48d15b20c0c4
Jean Landim <jewanbb@gmail.com>
2020-03-21 21:43:49 -0300

    static fixing

--- unica/settings.py
+++ unica/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#4gljrf)@sp=td57#50v4fwwkrpw62v%^ir#r&u4h#mf#wypb$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['unicaestetica.herokuapp.com']
+ALLOWED_HOSTS = ['unicaestetica.herokuapp.com','localhost']
 
 # Application definition
 
